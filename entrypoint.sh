5f72bb71bf053b6f65264406fa9502006a19affe
huy le <huy.dinh.le.89@gmail.com>
2018-11-27 11:53:59 -0800

    kubelogin for airmap

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo $KUBECONFIG
+echo $CLIENT_SECRET
+echo $CLIENT_ID
+
+CLUSTER=$(kubectl config current-context)
+kubectl config set-context $CLUSTER --cluster=$CLUSTER --user=oidc-user
+kubectl config set-credentials oidc-user  \
+          --auth-provider=oidc  \
+          --auth-provider-arg=idp-issuer-url=https://openid-connect.onelogin.com/oidc \
+          --auth-provider-arg=client-id=$CLIENT_ID \
+          --auth-provider-arg=client-secret=$CLIENT_SECRET 
+kubelogin
